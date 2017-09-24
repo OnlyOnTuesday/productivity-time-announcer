a1f3ed97bd0d7cb8c7c29b539a5ffcab844a906d
Michael <mikeycooney12@gmail.com>
2017-09-23 20:03:27 -0400

    created announce.py

--- timeApp/announcer.py
+++ timeApp/announcer.py
@@ -6,4 +6,15 @@ TIME = time.time()
 
 CTIME = time.ctime()
 
-def
+#How often should the alarm sound
+def user_config(hours default = 0, minutes default = 0, seconds default = 0):
+    var = time.time() - 60
+    one_minute = time.time - var
+    minutes *= 60
+    hours *= 3600 
+    interval = seconds + minutes + hours
+    return interval
+
+def alarm(interval):
+    #fix interval in user_config
+    #play sound files at interval
