2411a83d2a7f2d99bb363a091549a107b383c7dc
Michael <mikeycooney12@gmail.com>
2017-09-25 19:57:44 -0400

    changed .mp3 files to .wav and added wavPlayer file

--- timeApp/announcer.py
+++ timeApp/announcer.py
@@ -1,20 +1,25 @@
 #will keep track of time and make an announcement at a set interval
 
 import time
+from playsound import playsound
 
+#outputs the time in seconds since the epoch format
 TIME = time.time()
 
-CTIME = time.ctime()
+#outputs the time in hour : minute : second format
+LOCALTIME =str( time.localtime().tm_hour ) + " : " + str(time.localtime().tm_min) + " : " + str(time.localtime().tm_sec )
 
 #How often should the alarm sound
 def user_config(hours default = 0, minutes default = 0, seconds default = 0):
     var = time.time() - 60
     one_minute = time.time - var
     minutes *= 60
-    hours *= 3600 
+    hours *= 3600
     interval = seconds + minutes + hours
     return interval
 
 def alarm(interval):
+    if LOCALTIME
+
     #fix interval in user_config
     #play sound files at interval
