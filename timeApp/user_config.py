8f733df29eebf60181bd041a5ad01fb577fd6d7b
Michael <mikeycooney12@gmail.com>
2017-09-28 11:19:25 -0400

    modifed user_config.py to make user input usable to determine when the alarm should start

--- timeApp/user_config.py
+++ timeApp/user_config.py
@@ -1,21 +1,26 @@
 #user configuration for alarm settings
 
+import time
+
 #When should the alarm sound
 def user_config():
     user_input = input("""Please enter when you would like the alarm to sound. \n
-      Use the 'hour, minute, second' format""" end = " ")
+      Use the 'hour, minute, second, am/pm' format""" end = " ")
     #turn user_input into usable variables
-    hours, minutes,seconds = determine_variable(user_input)
-    #convert user_input into seconds
-    hours *= 3600
-    minutes *= 60
+    hours, minutes, seconds = determine_variable(user_input)
+    #gather the current asctime
+    current_hour, current_minute, current_second = split_time()
+    #how long until the alarm should begin
+    start_hour = hours - current_hour
+    start_minute = minutes - current_minute
+    start_second = seconds - current_second
+    #call on function to convert all to seconds and begin countdown 
+
     #make a countdown to keep track of how long it has been since the last alarm
     #based off of user input
     ##
     #Have user input how often it should go off
-    #Have user input days on which it should go off 
-
-
+    #Have user input days on which it should go off
 
 def determine_variable(user_input):
     #split the input into a list of integers
@@ -23,16 +28,25 @@ def determine_variable(user_input):
     hours = int(config_list[0])
     minutes = int(config_list[1])
     seconds = int(config_list[3])
+    #Did they type am or pm
+    time_of_day = config_list[4]
+    #get army time
+    hours = army_time(time_of_day, hours)
     return hours, minutes, seconds
 
+def split_time():
+    #take the asctime and exclude the extraneous output
+    #(day, Month, day of month, and year)
+    atime = time.asctime().split(" ")
+    atime = atime[3]
+    atime = atime.split(":")
+    return atime
 
-"""
-    #code that I forget the point of
-    #doubt its still useful, have changed approach
-    var = time.time() - 60s
-    one_minute = time.time - var
-    minutes *= 60
-    hours *= 3600
-    interval = seconds + minutes + hours
-    return interval
-"""
+def army_time(time_of_day, hours):
+    #convert time to army time
+    if time_of_day == "pm":
+        extra_hours = 24 - hours
+        hours += extra_hours
+        return hours
+    elif time_of_day == "am":
+        return hours
