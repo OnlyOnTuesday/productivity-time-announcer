41d92a35de1f989eb162ad2b4cb248df747b6a34
Michael <mikeycooney12@gmail.com>
2017-10-16 20:50:42 -0400

    added PyAudio as local directory, began debugging user_config.py

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # productivity-time-announcer
 
-#============================
+#========================================================================
 
 #An annoying app that will alert the user of the time at a given interval
+#Currently only works on an hourly basis
+
+#USAGE
+#CURRENTLY DEBUGGING
+#type command "python user_config.py" into terminal and follow instructions
--- timeApp/user_config.py
+++ timeApp/user_config.py
@@ -4,12 +4,14 @@ import time
 
 #When should the alarm sound
 def user_config():
-    user_input = input("""Please enter when you would like the alarm to sound. \n
-    Use the 'hour, minute, second, am/pm' format""")
+    user_input = input("""Please enter when you would like the alarm to sound.
+    Use the 'hour, minute, second, am/pm' format: """)
     #turn user_input into usable variables
     hours, minutes, seconds = determine_variable(user_input)
+    #hours, minutes, seconds = int(hours), int(minutes), int(seconds)
     #gather the current asctime
     current_hour, current_minute, current_second = split_time()
+    current_hour, current_minute, current_second = int(current_hour), int(current_minute), int(current_second)
     #how long until the alarm should begin
     start_hour = hours - current_hour
     start_minute = minutes - current_minute
@@ -27,11 +29,11 @@ def user_config():
 def determine_variable(user_input):
     #split the input into a list of integers
     config_list = user_input.split(",")
-    hours = int(config_list[0])
-    minutes = int(config_list[1])
-    seconds = int(config_list[3])
+    hours = config_list[0]
+    minutes = config_list[1]
+    seconds = config_list[2]
     #Did they type am or pm
-    time_of_day = config_list[4]
+    time_of_day = config_list[3]
     #get army time
     hours = army_time(time_of_day, hours)
     return hours, minutes, seconds
@@ -47,11 +49,12 @@ def split_time():
 def army_time(time_of_day, hours):
     #convert time to army time
     if time_of_day == "pm":
+        hours = int(hours)
         extra_hours = 24 - hours
         hours += extra_hours
         return hours
     elif time_of_day == "am":
-        return hours
+        return int(hours)
 
 def seconds_time(start_hour, start_minute, start_second):
     #convert all hours and minutes to seconds
@@ -61,3 +64,5 @@ def seconds_time(start_hour, start_minute, start_second):
     #of the alarm in seconds
     seconds_till_start = start_hour + start_minute + start_second
     return seconds_till_start
+
+user_config()
